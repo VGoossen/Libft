1eba01f6d0e045d47fdfa9b3ad0a1ad370975012
Vic Goossens <vgoossen@a2-r3-p4.s19.be>
2023-10-17 15:02:15 +0200

    next

---
+++ ft_memmove.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgoossen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 12:22:38 by vgoossen          #+#    #+#             */
+/*   Updated: 2023/10/17 13:33:24 by vgoossen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*memmove(void *dest, const void *src, size_t n)
+{
+	char *s;
+	char *d;
+	size_t		i;
+
+	s = (char *)src;
+	d = (char *)dest;
+	i = 0;
+
+	if (d = s)
+		return (dest);
+	if (d > s)
+		while (len-- > 0)
+			d[len] = s[len];
+	else
+		while (i < len)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	return (dest);
+}
